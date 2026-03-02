617d485a16b619b22f66765cc3de42844097100e
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2026-03-02 10:28:40 +0530

    Implement factory-based MyCustomMiddlewae

--- MyCustomMiddleware.cs
+++ MyCustomMiddleware.cs
@@ -1,26 +1,20 @@
 namespace AspNetCoreMiddleware;
 
-public class MyCustomMiddleware
+public class MyCustomMiddleware : IMiddleware
 {
-    private readonly RequestDelegate _next;
-
-    public MyCustomMiddleware(RequestDelegate next)
-    {
-        _next = next;
-    }
-
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         context.Request.Headers.Append(
             "X-Timestamp",
             DateTime.Now.ToLongTimeString()
         );
+
         context.Response.Headers.Append(
             "X-Content-Type-Options",
             "nosniff"
         );
 
-        await _next(context);
+        await next(context);
     }
 }
 
--- obj/Debug/net9.0/RazorMiddlewareApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorMiddlewareApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorMiddlewareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee91c636fd3172a71abc89407dc2854f323fa2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bffa8fd59732f8ed490ccb0102099b77e6e58bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorMiddlewareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorMiddlewareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
