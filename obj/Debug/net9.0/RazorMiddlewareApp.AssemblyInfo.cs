d85f6775a8d5932e55484379c285709518ecf648
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2026-03-02 09:36:20 +0530

    Add User-Agent based middleware using UseWhen

--- obj/Debug/net9.0/RazorMiddlewareApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorMiddlewareApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorMiddlewareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da16c1734466211311063123fddef3b60d097cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee30cc89a652e02d5ed75f47a3b626857c47609")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorMiddlewareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorMiddlewareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
